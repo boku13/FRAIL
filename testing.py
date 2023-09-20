e0171ac6b5ee515509aa98f4e6fc17fe08fbedfa
Vedant Funde <120516171+vedantfunde@users.noreply.github.com>
2023-09-20 13:59:15 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -16,33 +16,24 @@ def check_accuracy(loader, model):
     num_correct = 0
     num_samples = 0
     model.eval()
-
     # We don't need to keep track of gradients here so we wrap it in torch.no_grad()
     with torch.no_grad():
         # Loop through the data
         for x, y in loader:
-
             # Move data to device
             x = x.to(device=device)
             y = y.to(device=device)
-
             # Get to correct shape
             x = x.reshape(x.shape[0], -1)
-
             # Forward pass
             scores = model(x)
             _, predictions = scores.max(1)
-
             # Check how many we got correct
             num_correct += (predictions == y).sum()
-
             # Keep track of the number of samples
             num_samples += predictions.size(0)
-
     model.train()
     return num_correct / num_samples
-
-
 #Check accuracy on training & test to see how good our model
 model.to(device)
 print(f"Accuracy on training set: {check_accuracy(train_loader, model)*100:.2f}")
